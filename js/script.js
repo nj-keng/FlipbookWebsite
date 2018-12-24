29a363f54b36a2e7f6eb1cd357a668f5d52f9a6c
jeffreyh93 <jeffreyhuynh0@gmail.com>
2018-12-24 12:42:49 -0800

    added instagram feed css

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var userFeed = new Instafeed({
   clientId: '967cb53a4c2d469fbffb560075f1d0ce',
   accessToken: '6123706635.967cb53.bca597e9ca8c4b46aad2b1dd3244b794',
   resolution: 'standard_resolution',
-  template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+  template: '<div class="igFeed"><a href="{{link}}" target="_blank" id="{{id}}"><img class="igImage" src="{{image}}" /></a><p class="igCaptionLeft">{{likes}}</p> <p class="igCaptionRight">{{caption}}</p></div>',
   sortBy: 'most-recent',
   limit: 6,
   links: false,
